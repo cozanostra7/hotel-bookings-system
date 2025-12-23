15edf1ec390e5130d2cec0454c875113824939e7
coza_nostra7 <begzodmuminovb@gmail.com>
2025-12-23 19:43:34 +0100

    add get_all method for HotelsRepository to GET hotels

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -20,28 +20,15 @@ async def read_hotels(
         location: str | None = Query(None, description="location of the hotel"),
         title: str | None = Query(None, description="Hotel title"),):
 
+    per_page = pagination.per_page or 5
     async with async_session_maker() as session:
-       return await HotelsRepository(session).get_all()
-    # per_page = pagination.per_page or 5
-    # async with (async_session_maker() as session):
-    #     query = select(HotelsOrm)
-    #     if location:
-    #         query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
-    #     if title:
-    #         query= query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
-    #     query = (
-    #         query
-    #         .limit(per_page)
-    #         .offset(per_page * (pagination.page - 1)))
-    #     print(query.compile(compile_kwargs={"literal_binds":True}))
-    #     result = await session.execute(query)
-    #
-    #     hotels = result.scalars().all()
-    #     #print(type(hotels),hotels)
-    #     return hotels
+       return await HotelsRepository(session).get_all(
+           location = location,
+           title = title,
+           limit=per_page,
+           offset=per_page * (pagination.page - 1))
 
-    #if pagination.page and pagination.per_page:
-    #return [hotel for hotel in hotels if hotel['title']==title and hotel['id'] == id]
+
 
 
 @router.post('')
