c377bb558fd367f9b6590b6c02415e2e9a4ad143
coza_nostra7 <begzodmuminovb@gmail.com>
2026-01-18 20:37:15 +0100

    example with fastapi infinite async task

--- src/api/images.py
+++ src/api/images.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Body,UploadFile
+from fastapi import APIRouter, Body, UploadFile, BackgroundTasks
 import shutil
 
 from src.tasks.tasks import resize_image
@@ -7,10 +7,11 @@ router = APIRouter(prefix='/images',tags=['Images of hotels'])
 
 
 @router.post('')
-async def upload_image(file:UploadFile):
+async def upload_image(file:UploadFile,background_tasks: BackgroundTasks):
     image_path = f'src/static/images/{file.filename}'
     with open(image_path,'wb+') as new_file:
         shutil.copyfileobj(file.file,new_file)
 
 
-    resize_image.delay(image_path)
+    #resize_image.delay(image_path)
+    background_tasks.add_task(resize_image,image_path)
