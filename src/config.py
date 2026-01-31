7e0b6e5da88781c84f7498391e6b095fdeda5d54
coza_nostra7 <begzodmuminovb@gmail.com>
2026-01-31 22:19:04 +0100

    create folder tests,write tests. Create env-test environment for tests on database

--- src/config.py
+++ src/config.py
@@ -1,12 +1,14 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings,SettingsConfigDict
 
 class Settings(BaseSettings):
+    MODE: Literal['TEST','LOCAL','DEV','PROD']
     DB_NAME:str
     DB_HOST: str
     DB_PORT: int
     DB_PASS: str
     DB_USER:str
-
     REDIS_HOST : str
     REDIS_PORT : int
 
