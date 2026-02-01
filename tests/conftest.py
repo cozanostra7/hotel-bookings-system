6545c234fb32a76613bfb113512c27c59c79f023
coza_nostra7 <begzodmuminovb@gmail.com>
2026-02-01 10:53:00 +0100

    fill the database(hotels,rooms) with data. Write a function to fill the database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,17 @@
-from http.client import responses
+import json
+from pathlib import Path
 
 import pytest
 
 from src.config import settings
-from src.database import Base, engine_null_pool
+from src.database import Base, engine_null_pool, async_session_maker_null_poll, async_session_maker
 from src.main import app
 from src.models import *
 from httpx import AsyncClient
+
+from src.schemas.hotels import HotelAdd
+from src.schemas.rooms import RoomAdd
+from src.utils.db_manager import DBManager
 
 
 @pytest.fixture(scope='session',autouse=True)
@@ -20,6 +25,22 @@ async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+        data_path = Path('tests/')
+
+        with (data_path / 'mock_hotels.json').open(encoding='utf-8') as f:
+            hotels= json.load(f)
+
+        with (data_path / 'mock_rooms.json').open(encoding='utf-8') as f:
+            rooms = json.load(f)
+
+        hotels = [HotelAdd.model_validate(hotel) for hotel in hotels]
+        rooms = [RoomAdd.model_validate(room) for room in rooms]
+
+        async with DBManager(session_factory=async_session_maker_null_poll) as db:
+            db.hotels.add_bulk(hotels)
+            db.rooms.add_bulk(rooms)
+            await db.commit()
 
 
 @pytest.fixture(scope='session',autouse=True)
